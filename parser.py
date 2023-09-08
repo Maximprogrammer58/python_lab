2e3ae8e8005c882af652685191c63183cbd1f0fd
Maximprogrammer58 <maks.shamen2004@mail.ru>
2023-09-09 00:30:04 +0400

    data collection with automatically generated urls.

--- parser.py
+++ parser.py
@@ -4,28 +4,23 @@ import os
 import requests
 
 
-def month_conversion(month):
-    months_list = {"Январь": "01", "Февраль": "02", "Март": "03", "Апрель": "04", "Май": "05", "Июнь": "06",
-                   "Июль": "07", "Август": "08", "Сентябрь": "09", "Октябрь": "10", "Ноябрь": "11", "Декабрь": "12"}
-    return months_list.get(month)
-
-
-def parser():
-    URL = "https://www.gismeteo.ru/diary/4618/2019/9/"
-    html_page = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
-    soup = BeautifulSoup(html_page.text, 'lxml')
-
+def parser(year_from, year_to, step=1):
     parser_data = []
+    
+    for year in range(year_from, year_to + 1, step):
+        for month in range(1, 13):
+            URL = f"https://www.gismeteo.ru/diary/4618/{year}/{month}/"
+            html_page = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
+            soup = BeautifulSoup(html_page.text, 'lxml')
 
-    month_and_year = soup.title.text.split()
-    month, year = month_conversion(month_and_year[7]), month_and_year[8]
-
-    for day in soup.find_all('td', class_="first"):
-        temp = day.find_next()
-        press = temp.find_next()
-        wind = press.find_next_sibling().find_next_sibling().find_next_sibling()
-        parser_data.append([day.text + "." + month + "." + year,
-                           temp.text+"°C", press.text+"мм.рт.ст.", wind.text])
+            for day in soup.find_all('td', class_="first"):
+                try:
+                    temp = day.find_next()
+                    press = temp.find_next()
+                    wind = press.find_next_sibling().find_next_sibling().find_next_sibling()
+                    parser_data.append([day.text + "." + str(month).zfill(2) + "." + str(year), temp.text + "°C", press.text + "мм.рт.ст.", wind.text])
+                except:
+                    print("Parsing error")
 
     return parser_data
 
@@ -36,4 +31,6 @@ def upload_csv(parser_data):
         writer.writerows(parser_data)
 
 
-upload_csv(parser())
+upload_csv(parser(2008, 2023))
+
+print("Загрузка завершена")
