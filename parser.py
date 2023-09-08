b8efdb1035bbb26db4bc58ff9eb93788a6fb21f6
Maximprogrammer58 <maks.shamen2004@mail.ru>
2023-09-08 23:02:07 +0400

    mplementation of functions. Creating and filling in a csv file with data

--- parser.py
+++ parser.py
@@ -1,21 +1,39 @@
 from bs4 import BeautifulSoup
-import requests
+import csv
 import os
+import requests
 
-parser_data_list = []
 
-URL = "https://www.gismeteo.ru/diary/4618/2023/8/"
-html_page = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
-soup = BeautifulSoup(html_page.text, 'lxml')
+def month_conversion(month):
+    months_list = {"Январь": "01", "Февраль": "02", "Март": "03", "Апрель": "04", "Май": "05", "Июнь": "06",
+                   "Июль": "07", "Август": "08", "Сентябрь": "09", "Октябрь": "10", "Ноябрь": "11", "Декабрь": "12"}
+    return months_list.get(month)
 
-month_and_year = soup.title.text.split()
-month, year = month_and_year[7], month_and_year[8]
 
-for element in soup.find_all('td', class_="first"):
-    temp = element.find_next()
-    press = temp.find_next()
-    wind = press.find_next_sibling().find_next_sibling().find_next_sibling()
-    parser_data_list.append([[int(year), month, int(element.text)], [
-        temp.text, press.text, wind.text]])
+def parser():
+    URL = "https://www.gismeteo.ru/diary/4618/2019/9/"
+    html_page = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
+    soup = BeautifulSoup(html_page.text, 'lxml')
 
-print(parser_data_list)
+    parser_data = []
+
+    month_and_year = soup.title.text.split()
+    month, year = month_conversion(month_and_year[7]), month_and_year[8]
+
+    for day in soup.find_all('td', class_="first"):
+        temp = day.find_next()
+        press = temp.find_next()
+        wind = press.find_next_sibling().find_next_sibling().find_next_sibling()
+        parser_data.append([day.text + "." + month + "." + year,
+                           temp.text+"°C", press.text+"мм.рт.ст.", wind.text])
+
+    return parser_data
+
+
+def upload_csv(parser_data):
+    with open('dataset.csv', 'w', encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(parser_data)
+
+
+upload_csv(parser())
